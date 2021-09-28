6ba01f980430baace193b0fdc8d99043fd6dbfd6
wangzheng <zheng.wang@fih-foxconn.com>
2021-09-28 14:56:36 +0800

    add proxy

---
+++ src/generators/react17_antd4.0/config/proxy.js
@@ -0,0 +1,49 @@
+// module.exports = function (app) {
+//   app.use(
+//     '/admindevice',
+//     createProxyMiddleware({
+//       target: 'http://121.41.109.132',
+//       changeOrigin: true,
+//       pathRewrite: { '^/admindevice': '/admindevice' },
+//     })
+//   );
+// };
+
+module.exports = {
+  /* *********************** DEVICE SERVICE *********************** */
+  /*
+   * Fox Example:
+   * GetDeviceList: http://121.41.109.132/admindevice/device/deviceInfo/selectPage
+   *
+   */
+  '/admindevice': {
+    target: 'http://121.41.109.132',
+    changeOrigin: true,
+    pathRewrite: { '^/admindevice': '/admindevice' },
+  },
+
+  /* *********************** ICS SERVICE *********************** */
+  /*
+   * Fox Example:
+   * Login: http://47.56.210.117:48279/oauth/token
+   * Register: http://47.56.210.117:48279/user/register
+   *
+   */
+  '/ics': {
+    target: 'http://47.56.210.117:48279',
+    changeOrigin: true,
+    pathRewrite: { '^/ics': '' },
+  },
+
+  /* *********************** FUS SERVICE *********************** */
+  /*
+   * Fox Example:
+   * GetDownloadSignedUrl: http://121.40.88.181:16088/fus/common/getDownloadSignedUrl
+   *
+   */
+  '/fus': {
+    target: 'http://121.40.88.181:16088',
+    changeOrigin: true,
+    pathRewrite: { '^/fus': '' },
+  },
+};
