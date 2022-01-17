c10a92f7438995c7dfff7814ba987cc2288e60ab
wangzheng <zheng.wang@fih-foxconn.com>
2022-01-17 11:22:00 +0800

    init the app

---
+++ src/generators/vue2_view-design4.0/vue.config.js
@@ -0,0 +1,24 @@
+const { resolve } = require('path');
+
+const { VUE_APP_PUBLIC_PATH } = process.env.VUE_APP_PUBLIC_PATH;
+module.exports = {
+  publicPath: VUE_APP_PUBLIC_PATH,
+  outputDir: 'dist',
+  productionSourceMap: false,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [resolve(__dirname, 'src/theme/common.less')],
+    },
+  },
+  css: { loaderOptions: { less: { javascriptEnabled: true } } },
+  lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port: 8080,
+    // proxy: {},
+    overlay: { warnings: false, errors: true },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', resolve(__dirname, 'src/'));
+  },
+};
