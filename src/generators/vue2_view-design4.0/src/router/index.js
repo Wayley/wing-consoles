ae4b667b3a84273e7528f27347a5e03941831f69
wangzheng <zheng.wang@fih-foxconn.com>
2022-01-17 14:40:26 +0800

    add LoadingBar

--- src/generators/vue2_view-design4.0/src/router/index.js
+++ src/generators/vue2_view-design4.0/src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import ViewUI from 'view-design';
 
 Vue.use(VueRouter);
 export const router = new VueRouter({
   routes,
 });
-
+router.beforeEach((to, from, next) => {
+  ViewUI.LoadingBar.start();
+  next();
+});
+router.afterEach(() => {
+  ViewUI.LoadingBar.finish();
+  window.scrollTo(0, 0);
+});
 export default router;
