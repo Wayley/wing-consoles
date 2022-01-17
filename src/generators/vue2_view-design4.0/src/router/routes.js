ebb5bdab39c507bb8927c9e1a8bc35720e2e780a
wangzheng <zheng.wang@fih-foxconn.com>
2022-01-17 14:32:56 +0800

    add 404 page

--- src/generators/vue2_view-design4.0/src/router/routes.js
+++ src/generators/vue2_view-design4.0/src/router/routes.js
@@ -1,5 +1,6 @@
 export const routes = [
   { path: '/login', component: () => import('@/views/basic/Login.vue') },
   { path: '/', component: () => import('@/views/basic/Welcome.vue') },
+  { path: '*', component: () => import('@/views/basic/NoMatch.vue') },
 ];
 export default routes;
---
+++ src/generators/vue2_view-design4.0/src/views/basic/NoMatch.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="no-match-page">404</div>
+</template>
+
+<script>
+export default {
+  name: 'NoMatch',
+};
+</script>
+
+<style lang="less"></style>
