1140d12392d2b01d65d21315483ae88c964e4d56
wangzheng <zheng.wang@fih-foxconn.com>
2022-01-20 17:17:54 +0800

    add login page

--- src/generators/vue2_view-design4.0/src/config/index.js
+++ src/generators/vue2_view-design4.0/src/config/index.js
@@ -3,3 +3,18 @@ export const TOKEN_KEY = `${APP_PREFIX}_token`;
 export const LOCALE_KEY = `${APP_PREFIX}_locale`;
 export const USER_NAME_KEY = `${APP_PREFIX}_user_name`;
 export const USER_ID_KEY = `${APP_PREFIX}_user_id`;
+
+/* *************************** CODETYPE CONFIG *************************** */
+export const CodeType = {
+  SUCCESS: 200,
+  isSuccess: (code) => code === CodeType.SUCCESS,
+
+  UNAUTHORIZED: 401,
+  isUnAuthorized: (code) => code === CodeType.UNAUTHORIZED,
+
+  FORBIDDEN: 403,
+  isForbiddened: (code) => code === CodeType.FORBIDDEN,
+
+  TIMEOUT: 408,
+  isTimeout: (code) => code === CodeType.TIMEOUT,
+};
--- src/generators/vue2_view-design4.0/src/views/basic/Login.vue
+++ src/generators/vue2_view-design4.0/src/views/basic/Login.vue
@@ -1,23 +1,97 @@
 <template>
   <div class="login-page">
-    login-page
-    <p>User Locale: {{ locale }}</p>
-    <p>User Channel Model Id: {{ channelModelId }}</p>
-
-    <Button @click="updateLocale('en_US')">Set Locale As `en_US`</Button>
+    <Form ref="form" :model="formData" :rules="rules" :label-width="80">
+      <FormItem label="Username" prop="username">
+        <Input v-model="formData.username" />
+      </FormItem>
+      <FormItem label="Password" prop="password">
+        <Input type="password" password v-model="formData.password" />
+      </FormItem>
+      <FormItem label="VerifyCode" prop="verifyCode">
+        <Input v-model="formData.verifyCode" />
+        <img :src="verifyCode" alt="verify code" @click="updateVerifyCode" />
+      </FormItem>
+      <FormItem>
+        <Button
+          type="primary"
+          style="width: 100%"
+          @click="handleSubmit('form')"
+          :loading="loading"
+        >
+          {{ loading ? 'Logging' : 'Log In' }}
+        </Button>
+      </FormItem>
+    </Form>
   </div>
 </template>
-
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapActions } from 'vuex';
+import { CodeType } from '@/config';
+const API_PATH = process.env.VUE_APP_CONSOLE_SERVICE_PATH;
+
+const validateFactory = (keyName) => (rule, value, callback) => {
+  if (value === '') callback(new Error(`${keyName} cannot be empty.`));
+  callback();
+};
+const createForm = (object) => {
+  let form = new FormData();
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      form.append(key, object[key]);
+    }
+  }
+  return form;
+};
 export default {
   name: 'Login',
-  computed: mapState({
-    locale: (state) => state.user.locale,
-    channelModelId: (state) => state.app.channel.channelModelId,
-  }),
-  methods: mapActions('user', ['updateLocale']),
+  data() {
+    return {
+      formData: { username: '', password: '', verifyCode: '' },
+      rules: {
+        username: [{ validator: validateFactory('username'), trigger: 'blur' }],
+        password: [{ validator: validateFactory('password'), trigger: 'blur' }],
+        verifyCode: [
+          { validator: validateFactory('verifyCode'), trigger: 'blur' },
+        ],
+      },
+      loading: false,
+      verifyCode: `${API_PATH}/verification`,
+    };
+  },
+  methods: {
+    ...mapActions('user', ['loggedIn']),
+    updateVerifyCode() {
+      this.verifyCode = `${API_PATH}/verification?t=${new Date().getTime()}`;
+    },
+    handleSubmit(name) {
+      this.$refs[name].validate(async (valid) => {
+        if (valid) {
+          this.loading = true;
+          const response = await fetch(`${API_PATH}/login`, {
+            method: 'POST',
+            body: createForm(this.formData),
+            credentials: 'include',
+          });
+          const result = await response.json();
+          if (result) {
+            const { code, data, message } = result;
+            if (CodeType.isSuccess(code)) {
+              this.loggedIn(data);
+              this.$router.push({ name: 'home' });
+            } else {
+              this.$Message.error(message);
+            }
+          }
+          this.loading = false;
+        }
+      });
+    },
+  },
 };
 </script>
-
-<style lang="less"></style>
+<style lang="less">
+.login-page {
+  width: 360px;
+  margin: 200px auto;
+}
+</style>
