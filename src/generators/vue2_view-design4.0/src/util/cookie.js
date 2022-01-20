fe6afada6f8232e7cdeac64f4fc929c80f386b8c
wangzheng <zheng.wang@fih-foxconn.com>
2022-01-20 15:12:21 +0800

    add cookie utils

--- src/generators/vue2_view-design4.0/src/util/cookie.js
+++ src/generators/vue2_view-design4.0/src/util/cookie.js
@@ -1,10 +1,23 @@
-import Cookies from 'js-cookie';
+import JS_COOKIE from 'js-cookie';
 
-export const cookie = Cookies;
+const path = process.env.VUE_APP_PUBLIC_PATH;
+const expires = 1; // as day
 
-export const _cookie = Cookies.withAttributes({
-  path: process.env.VUE_APP_PUBLIC_PATH,
-  expires: 1, // as day
-});
+export const INSTANCE = JS_COOKIE.withAttributes({ path, expires });
 
-export default Cookies;
+export const removeAll = () => {
+  Object.keys(JS_COOKIE.get()).forEach((key) => {
+    JS_COOKIE.remove(key, { path });
+  });
+};
+export const multipleSet = (data = {}) => {
+  for (const key in data) {
+    if (Object.hasOwnProperty.call(data, key)) {
+      JS_COOKIE.set(key, data[key], { path, expires });
+    }
+  }
+};
+
+export const _cookie = JS_COOKIE;
+
+export default _cookie;
